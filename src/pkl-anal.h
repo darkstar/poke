5dc2d5423cbec248f9214d170db4f6780e9639ad
Jose E. Marchesi <jose.marchesi@oracle.com>
2018-03-24 03:19:18 +0100

    foo

---
+++ src/pkl-anal.h
@@ -0,0 +1,31 @@
+/* pkl-anal.h - Analysis phase for the poke compiler.  */
+
+/* Copyright (C) 2018 Jose E. Marchesi */
+
+/* This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef PKL_ANAL_H
+#define PKL_ANAL_H
+
+#include <config.h>
+
+struct pkl_anal_payload
+{
+  int errors;
+};
+
+typedef struct pkl_anal_payload *pkl_anal_payload;
+
+#endif /* PKL_ANAL_H */
