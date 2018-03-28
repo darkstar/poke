0619f02d6e0e82ba41844a8c07390e3b200fdad6
Jose E. Marchesi <jose.marchesi@oracle.com>
2018-03-29 01:04:30 +0200

    foo

--- src/pk-term.h
+++ src/pk-term.h
@@ -22,6 +22,8 @@
 /* Define ANSI terminal escape sequences to print each object with
    attributes depending on its tag. */
 
+#if 0
+
 #define ESC        "\033"
 #define NOATTR     ESC"[0m"
 #define BOLD       ESC"[1m"
@@ -48,5 +50,36 @@
 #define DARKGRAY     ESC"[1m"ESC"[30m"
 #define LIGHTBLUE    ESC"[1m"ESC"[34m"
 #define YELLOW       ESC"[1m"ESC"[33m"
+
+#else
+
+#define ESC
+#define NOATTR
+#define BOLD
+#define FAINT
+#define ITALIC
+#define UNDERLINE
+#define REVERSE
+#define CROSSOUT
+
+#define BLACK
+#define WHITE
+#define BLUE
+#define LIGHTBLUE
+#define GREEN
+#define LIGHTGREEN
+#define CYAN
+#define LIGHTCYAN
+#define RED
+#define LIGHTRED
+#define MAGENTA
+#define LIGHTMAGENTA
+#define BROWN
+#define LIGHTGRAY
+#define DARKGRAY
+#define LIGHTBLUE
+#define YELLOW
+
+#endif
 
 #endif /* PK_TERM_H */
