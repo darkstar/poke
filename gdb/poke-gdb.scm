e5783a54f4ea89b86a792a8ae94484258526806b
Jose E. Marchesi <jose.marchesi@oracle.com>
2018-04-05 11:51:23 +0200

    foo

--- gdb/poke-gdb.scm
+++ gdb/poke-gdb.scm
@@ -22,16 +22,63 @@
 (define (make-poke-pvm-val-printer value)
   "Print a pvm_val object"
   (make-pretty-printer-worker
-   "pvm_val"
+   #f
    (lambda (printer)
-     (value-field value "foo"))
+     (let ((pvm-tag (value-logand value #b111)))
+       (case (value->integer pvm-tag)
+         ((#x0) ;; PVM_VAL_TAG_INT
+          (let* ((int-size (value-add (value-cast (value-logand (value-rsh value 3) #x1f)
+                                                  (lookup-type "int32_t"))
+                                      1))
+                 (int-val (value-rsh (value-lsh (value-cast
+                                                 (value-rsh value 32)
+                                                 (lookup-type "int32_t"))
+                                                (value-sub 32 int-size))
+                                     (value-sub 32 int-size))))
+            (format #f "(pvm:int<~a>) ~a" int-size int-val)))
+         ((#x1) ;; PVM_VAL_TAG_UINT
+          (let* ((uint-size (value-add (value-cast (value-logand (value-rsh value 3) #x1f)
+                                                   (lookup-type "int32_t"))
+                                       1))
+                 (uint-val (value-logand (value-cast
+                                          (value-rsh value 32)
+                                          (lookup-type "uint32_t"))
+                                         (value-cast
+                                          (value-lognot
+                                           (value-lsh
+                                            (value-lsh
+                                             (value-lognot
+                                              (value-cast (make-value 0)
+                                                          (lookup-type "uint64_t")))
+                                             (value-sub uint-size 1))  
+                                            (make-value 1)))
+                                          (lookup-type "uint32_t")))))
+            (format #f "(pvm:uint<~a>) ~a" uint-size uint-val)))
+         ((#x2) ;; PVM_VAL_TAG_LONG
+          (let ((long-size #t)
+                (long-val #t))
+            (format #f "(pvm:int<~a>) ~a" long-size long-val)))
+         ((#x3) ;; PVM_VAL_TAG_ULONG
+          (let ((ulong-size #t)
+                (ulong-val #t))
+            (format #f "(pvm:uint<~a>) ~a" ulong-size ulong-val)))
+         ((#x6) ;; PVM_VAL_TAG_BOX
+          (let ((pvm_ptr (value-cast (value-rsh value 3)
+                                     (lookup-type "pvm_val_box"))))
+            "A BOX"))
+         ((#x7) ;; PVM_NULL
+          "PVM_NULL")
+         (else
+          "Unknown PVM_VAL tag"))))
    #f))
 
-(define (poke-pvm-val-lookup-function pretty-printer value)
-  (let ((tag (type-tag (value-type value))))
-    (and tag
-         (string-prefix? "pvm_val" tag)
+(define (poke-lookup-function pretty-printer value)
+  (let ((tname (type-name (value-type value))))
+    (and tname
+         (equal? tname "pvm_val")
          (make-poke-pvm-val-printer value))))
 
-(append-pretty-printer!
- (make-pretty-printer "pvm_val" poke-pvm-val-lookup-function))
+(set-pretty-printers!
+ (cons
+  (make-pretty-printer "pvm_val" poke-lookup-function)
+  (pretty-printers)))
