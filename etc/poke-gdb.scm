7bebf77cace4efc3c7e904c1b607b4298ee3d3cc
Jose E. Marchesi <jose.marchesi@oracle.com>
2018-04-05 13:42:28 +0200

    foo

--- etc/poke-gdb.scm
+++ etc/poke-gdb.scm
@@ -19,114 +19,122 @@
 
 ;; Pretty-printer for pvm_val objects.
 
+(define (pp-pvm-val value)
+  (let ((pvm-tag (value-logand value #b111)))
+    (case (value->integer pvm-tag)
+      ((#x0) ;; PVM_VAL_TAG_INT
+       (let* ((int-size (value-add (value-cast (value-logand (value-rsh value 3) #x1f)
+                                               (lookup-type "int32_t"))
+                                   1))
+              (int-val (value-rsh (value-lsh (value-cast
+                                              (value-rsh value 32)
+                                              (lookup-type "int32_t"))
+                                             (value-sub 32 int-size))
+                                  (value-sub 32 int-size))))
+         (format #f "(pvm:int<~a>) ~a" int-size int-val)))
+      ((#x1) ;; PVM_VAL_TAG_UINT
+       (let* ((uint-size (value-add (value-cast (value-logand (value-rsh value 3) #x1f)
+                                                (lookup-type "int32_t"))
+                                    1))
+              (uint-val (value-logand (value-cast
+                                       (value-rsh value 32)
+                                       (lookup-type "uint32_t"))
+                                      (value-cast
+                                       (value-lognot
+                                        (value-lsh
+                                         (value-lsh
+                                          (value-lognot
+                                           (value-cast (make-value 0)
+                                                       (lookup-type "uint64_t")))
+                                          (value-sub uint-size 1))  
+                                         (make-value 1)))
+                                       (lookup-type "uint32_t")))))
+         (format #f "(pvm:uint<~a>) ~a" uint-size uint-val)))
+      ((#x2) ;; PVM_VAL_TAG_LONG
+       (let* ((long-ulong-val (value-subscript
+                               (value-cast
+                                (value-logand (value-cast value (lookup-type "uintptr_t"))
+                                              (value-lognot #x7))
+                                (type-pointer (lookup-type "int64_t")))
+                               0))
+              (long-size (value-add
+                          (value-subscript
+                           (value-cast
+                            (value-logand (value-cast value (lookup-type "uintptr_t"))
+                                          (value-lognot #x7))
+                            (type-pointer (lookup-type "int64_t")))
+                           1)
+                          1))
+              (long-val (value-rsh (value-lsh
+                                    long-ulong-val
+                                    (value-sub 64 long-size))
+                                   (value-sub 64 long-size))))
+         (format #f "(pvm:long<~a>) ~a" long-size long-val)))
+      ((#x3) ;; PVM_VAL_TAG_ULONG
+       (let* ((long-ulong-val (value-subscript
+                               (value-cast
+                                (value-logand (value-cast value (lookup-type "uintptr_t"))
+                                              (value-lognot #x7))
+                                (type-pointer (lookup-type "int64_t")))
+                               0))
+              (ulong-size (value-add
+                           (value-subscript
+                            (value-cast
+                             (value-logand (value-cast value (lookup-type "uintptr_t"))
+                                           (value-lognot #x7))
+                             (type-pointer (lookup-type "int64_t")))
+                            1)
+                           1))
+              (ulong-val (value-logand
+                          long-ulong-val
+                          (value-cast (value-lognot
+                                       (value-lsh
+                                        (value-lsh
+                                         (value-lognot
+                                          (value-cast (make-value 0)
+                                                      (lookup-type "unsigned long long")))
+                                         (value-sub ulong-size 1))  
+                                        (make-value 1)))
+                                      (lookup-type "uint64_t")))))
+         (format #f "(pvm:ulong<~a>) ~a" ulong-size ulong-val)))
+      ((#x6) ;; PVM_VAL_TAG_BOX
+       (let* ((pvm-box-ptr (value-cast (value-logand
+                                        (value-cast value
+                                                    (lookup-type "uintptr_t"))
+                                        (value-lognot #x7))
+                                       (lookup-type "pvm_val_box")))
+              (pvm-box (value-dereference pvm-box-ptr))
+              (pvm-box-tag (value-field pvm-box "tag")))
+         (case (value->integer pvm-box-tag)
+           ((#x8) ;; PVM_VAL_TAG_STR
+            "STRING")
+           ((#x9) ;; PVM_VAL_TAG_OFF
+            (let ((offset (value-dereference (value-field (value-field pvm-box "v")
+                                                          "offset"))))
+              (format #f "(pvm:offset) [~a ~a]"
+                      (pp-pvm-val (value-field offset "magnitude"))
+                      (pp-pvm-val (value-field offset "unit")))))
+           ((#xa) ;; PVM_VAL_TAG_ARR
+            "ARRAY")
+           ((#xb) ;; PVM_VAL_TAG_SCT
+            "STRUCT")
+           ((#xc) ;; PVM_VAL_TAG_TYP
+            "TYPE")
+           ((#xd) ;; PVM_VAL_TAG_MAP
+            "MAP")
+           (else
+            "Unknown PVM_VAL_BOX tag"))))
+      ((#x7) ;; PVM_NULL
+       "PVM_NULL")
+      (else
+       "Unknown PVM_VAL tag"))))  
+
 (define (make-poke-pvm-val-printer value)
   "Print a pvm_val object"
   (make-pretty-printer-worker
    #f
    (lambda (printer)
-     (let ((pvm-tag (value-logand value #b111)))
-       (case (value->integer pvm-tag)
-         ((#x0) ;; PVM_VAL_TAG_INT
-          (let* ((int-size (value-add (value-cast (value-logand (value-rsh value 3) #x1f)
-                                                  (lookup-type "int32_t"))
-                                      1))
-                 (int-val (value-rsh (value-lsh (value-cast
-                                                 (value-rsh value 32)
-                                                 (lookup-type "int32_t"))
-                                                (value-sub 32 int-size))
-                                     (value-sub 32 int-size))))
-            (format #f "(pvm:int<~a>) ~a" int-size int-val)))
-         ((#x1) ;; PVM_VAL_TAG_UINT
-          (let* ((uint-size (value-add (value-cast (value-logand (value-rsh value 3) #x1f)
-                                                   (lookup-type "int32_t"))
-                                       1))
-                 (uint-val (value-logand (value-cast
-                                          (value-rsh value 32)
-                                          (lookup-type "uint32_t"))
-                                         (value-cast
-                                          (value-lognot
-                                           (value-lsh
-                                            (value-lsh
-                                             (value-lognot
-                                              (value-cast (make-value 0)
-                                                          (lookup-type "uint64_t")))
-                                             (value-sub uint-size 1))  
-                                            (make-value 1)))
-                                          (lookup-type "uint32_t")))))
-            (format #f "(pvm:uint<~a>) ~a" uint-size uint-val)))
-         ((#x2) ;; PVM_VAL_TAG_LONG
-          (let* ((long-ulong-val (value-subscript
-                                  (value-cast
-                                   (value-logand (value-cast value (lookup-type "uintptr_t"))
-                                                 (value-lognot #x7))
-                                   (type-pointer (lookup-type "int64_t")))
-                                  0))
-                 (long-size (value-add
-                             (value-subscript
-                              (value-cast
-                               (value-logand (value-cast value (lookup-type "uintptr_t"))
-                                             (value-lognot #x7))
-                               (type-pointer (lookup-type "int64_t")))
-                              1)
-                             1))
-                 (long-val (value-rsh (value-lsh
-                                       long-ulong-val
-                                       (value-sub 64 long-size))
-                                      (value-sub 64 long-size))))
-            (format #f "(pvm:long<~a>) ~a" long-size long-val)))
-         ((#x3) ;; PVM_VAL_TAG_ULONG
-          (let* ((long-ulong-val (value-subscript
-                                  (value-cast
-                                   (value-logand (value-cast value (lookup-type "uintptr_t"))
-                                                 (value-lognot #x7))
-                                   (type-pointer (lookup-type "int64_t")))
-                                  0))
-                 (ulong-size (value-add
-                              (value-subscript
-                                  (value-cast
-                                   (value-logand (value-cast value (lookup-type "uintptr_t"))
-                                                 (value-lognot #x7))
-                                   (type-pointer (lookup-type "int64_t")))
-                                  1)
-                              1))
-                 (ulong-val (value-logand
-                             long-ulong-val
-                             (value-cast (value-lognot
-                                          (value-lsh
-                                           (value-lsh
-                                            (value-lognot
-                                             (value-cast (make-value 0)
-                                                         (lookup-type "unsigned long long")))
-                                            (value-sub ulong-size 1))  
-                                           (make-value 1)))
-                                         (lookup-type "uint64_t")))))
-            (format #f "(pvm:ulong<~a>) ~a" ulong-size ulong-val)))
-         ((#x6) ;; PVM_VAL_TAG_BOX
-          (let* ((pvm-box-ptr (value-cast (value-logand
-                                          (value-cast value
-                                                      (lookup-type "uintptr_t"))
-                                          (value-lognot #x7))
-                                          (lookup-type "pvm_val_box")))
-                 (pvm-box-tag (value-field (value-dereference pvm-box-ptr) "tag")))
-            (case (value->integer pvm-box-tag)
-              ((#x8) ;; PVM_VAL_TAG_STR
-               "STRING")
-              ((#x9) ;; PVM_VAL_TAG_OFF
-               "OFFSET")
-              ((#xa) ;; PVM_VAL_TAG_ARR
-               "ARRAY")
-              ((#xb) ;; PVM_VAL_TAG_SCT
-               "STRUCT")
-              ((#xc) ;; PVM_VAL_TAG_TYP
-               "TYPE")
-              ((#xd) ;; PVM_VAL_TAG_MAP
-               "MAP")
-              (else
-               "Unknown PVM_VAL_BOX tag"))))
-         ((#x7) ;; PVM_NULL
-          "PVM_NULL")
-         (else
-          "Unknown PVM_VAL tag"))))
+     (pp-pvm-val value))
    #f))
 
 (define (poke-lookup-function pretty-printer value)
