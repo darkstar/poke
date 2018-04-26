e61860687b21e69e52cabff0cf229a53f69d4ad9
Jose E. Marchesi <jose.marchesi@oracle.com>
2018-04-26 23:08:26 +0200

    foo

--- src/poke.c
+++ src/poke.c
@@ -303,7 +303,7 @@ initialize ()
   poke_compiler = pkl_new ();
   if (!pkl_compile_file (poke_compiler,
                          /* XXX: use POKEDIR  */
-                         "/home/jemarch/gnu/hacks/poke/pickles/std.pk"))
+                         "/home/jemarch/gnu/hacks/poke/pk/std.pk"))
     exit (1);
 }
 
