1bedb0738cb32650b946ce92dd3bedffc140c91b
Jose E. Marchesi <jose.marchesi@oracle.com>
2019-02-08 11:27:10 +0100

    foo

--- src/pkl-asm.c
+++ src/pkl-asm.c
@@ -185,7 +185,8 @@ pkl_asm_insn_oto (pkl_asm pasm,
   pkl_ast_node to_base_unit = PKL_AST_TYPE_O_UNIT (to_type);
   pkl_ast_node to_base_unit_type = PKL_AST_TYPE (to_base_unit);
 
-  RAS_MACRO_OFFSET_CAST (from_base_type, from_base_unit_type, to_base_type, to_base_unit_type);
+  RAS_MACRO_OFFSET_CAST (from_base_type, from_base_unit_type, \
+                         to_base_type, to_base_unit_type);
 }
 
 /* Macro-instruction: NTON from_type, to_type
