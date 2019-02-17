f86c5a4895c12192905f2f71bdfdec565dc96796
Jose E. Marchesi <jose.marchesi@oracle.com>
2019-02-17 18:25:59 +0100

    foo

--- src/pkl-gen.c
+++ src/pkl-gen.c
@@ -626,10 +626,10 @@ PKL_PHASE_END_HANDLER
 PKL_PHASE_BEGIN_HANDLER (pkl_gen_ps_print_stmt)
 {
   pkl_ast_node print_stmt = PKL_PASS_NODE;
-  pkl_ast_node print_stmt_exp = PKL_AST_PRINT_STMT_ARGS (print_stmt);
+  pkl_ast_node print_stmt_args = PKL_AST_PRINT_STMT_ARGS (print_stmt);
 
   /* Print a newline if there is no expression.  */
-  if (!print_stmt_exp)
+  if (!print_stmt_args)
     pkl_asm_insn (PKL_GEN_ASM, PKL_INSN_PUSH, pvm_make_string ("\n"));
   pkl_asm_insn (PKL_GEN_ASM, PKL_INSN_PRINTS);
 }
