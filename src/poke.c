b608b07f1dc483440e64d5ffec424eeea8cc8b10
Jose E. Marchesi <jose.marchesi@oracle.com>
2019-07-03 15:42:09 +0200

    foo

--- src/poke.c
+++ src/poke.c
@@ -179,6 +179,9 @@ License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>.\n
 This is free software: you are free to change and redistribute it.\n\
 There is NO WARRANTY, to the extent permitted by law.\n"), "2019");
 
+  printf (_("\
+\nPowered by Jitter %s."), JITTER_VERSION);
+
   puts (_("\
 \n\
 Perpetrated by Jose E. Marchesi."));
