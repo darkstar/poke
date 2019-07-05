a3eaaef858935787c35a6b4557c559f05e73a2ed
Jose E. Marchesi <jose.marchesi@oracle.com>
2019-07-05 11:04:18 +0200

    foo

--- src/pkl-typify.c
+++ src/pkl-typify.c
@@ -1417,8 +1417,8 @@ PKL_PHASE_BEGIN_HANDLER (pkl_typify1_ps_scons)
 
               found = 1;
 
-              if (!pkl_ast_type_equal (elem_type, type_elem_type))
-                /* XXX support promo.  */
+              if (!pkl_ast_type_promoteable (elem_type, type_elem_type,
+                                             1 /* promote_array_of_any */))
                 {
                   char *expected_type = pkl_type_str (type_elem_type, 1);
                   char *found_type = pkl_type_str (elem_type, 1);
