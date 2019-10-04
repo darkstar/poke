b44297ccb2739060b76cd406c76c96141b49f0b4
Jose E. Marchesi <jose.marchesi@oracle.com>
2019-10-04 13:03:09 +0200

    maint: style fixes
    This makes all the checks in syntax-check to pass or being ignored.
    
    2019-10-04  Jose E. Marchesi  <jemarch@gnu.org>
    
    	* cfg.mk (local-checks-to-skip): Add sc_tight_scope.
    	* .x-sc_makefile_TAB_only_indentation: New file.
    	* .x-sc_prohibit_empty_lines_at_EOF: Likewise.
    	* .x-sc_makefile_path_separator_check: Likewise.
    	* .x-sc_prohibit_undesirable_word_seq: Likewise.
    	* .x-sc_tight_scope: Likewise.
    	* testsuite/poke.pkl/rand-1.pk: Remove empty lines at EOF.
    	* ChangeLog: Likewise.

--- cfg.mk
+++ cfg.mk
@@ -2,3 +2,7 @@ manual_title=GNU Poke
 old_NEWS_hash=d41d8cd98f00b204e9800998ecf8427e
 po_file=does-not-exist
 export _gl_TS_headers = *.h
+
+local-checks-to-skip =                  \
+   sc_tight_scope                       \
+   sc_trailing_blank
