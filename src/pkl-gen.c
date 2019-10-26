a18a5173b49c20b74c31d7e37ea6dad579ec4d34
Jose E. Marchesi <jose.marchesi@oracle.com>
2019-10-26 19:26:21 +0200

    pkl:gen: make && and || to use short-circuit evaluation
    2019-10-26  Jose E. Marchesi  <jemarch@gnu.org>
    
    	* src/pkl-gen.c (pkl_gen_pr_op_and): New handler.
    	(pkl_gen_pr_op_or): Likewise.
    	(pkl_gen_ps_op_and): Remove.
    	(pkl_gen_ps_op_or): Likewise.
    	* testsuite/poke.pkl/and-13.pk: New test.
    	* testsuite/poke.pkl/or-4.pk: Likewise.

--- src/pkl-gen.c
+++ src/pkl-gen.c
@@ -2371,17 +2371,37 @@ PKL_PHASE_BEGIN_HANDLER (pkl_gen_ps_op_intexp)
 }
 PKL_PHASE_END_HANDLER
 
-PKL_PHASE_BEGIN_HANDLER (pkl_gen_ps_op_and)
+PKL_PHASE_BEGIN_HANDLER (pkl_gen_pr_op_and)
 {
-  pkl_asm_insn (PKL_GEN_ASM, PKL_INSN_AND);
-  pkl_asm_insn (PKL_GEN_ASM, PKL_INSN_NIP2);
+  pkl_ast_node op1 = PKL_AST_EXP_OPERAND (PKL_PASS_NODE, 0);
+  pkl_ast_node op2 = PKL_AST_EXP_OPERAND (PKL_PASS_NODE, 1);
+
+  jitter_label label = pkl_asm_fresh_label (PKL_GEN_ASM);
+
+  PKL_PASS_SUBPASS (op1);
+  pkl_asm_insn (PKL_GEN_ASM, PKL_INSN_BZI, label);
+  pkl_asm_insn (PKL_GEN_ASM, PKL_INSN_DROP);
+  PKL_PASS_SUBPASS (op2);
+  pkl_asm_label (PKL_GEN_ASM, label);
+
+  PKL_PASS_BREAK;
 }
 PKL_PHASE_END_HANDLER
 
-PKL_PHASE_BEGIN_HANDLER (pkl_gen_ps_op_or)
+PKL_PHASE_BEGIN_HANDLER (pkl_gen_pr_op_or)
 {
-  pkl_asm_insn (PKL_GEN_ASM, PKL_INSN_OR);
-  pkl_asm_insn (PKL_GEN_ASM, PKL_INSN_NIP2);
+  pkl_ast_node op1 = PKL_AST_EXP_OPERAND (PKL_PASS_NODE, 0);
+  pkl_ast_node op2 = PKL_AST_EXP_OPERAND (PKL_PASS_NODE, 1);
+
+  jitter_label label = pkl_asm_fresh_label (PKL_GEN_ASM);
+
+  PKL_PASS_SUBPASS (op1);
+  pkl_asm_insn (PKL_GEN_ASM, PKL_INSN_BNZI, label);
+  pkl_asm_insn (PKL_GEN_ASM, PKL_INSN_DROP);
+  PKL_PASS_SUBPASS (op2);
+  pkl_asm_label (PKL_GEN_ASM, label);
+
+  PKL_PASS_BREAK;
 }
 PKL_PHASE_END_HANDLER
 
@@ -2687,8 +2707,8 @@ struct pkl_phase pkl_phase_gen =
    PKL_PHASE_PS_OP_HANDLER (PKL_AST_OP_SL, pkl_gen_ps_op_intexp),
    PKL_PHASE_PS_OP_HANDLER (PKL_AST_OP_SR, pkl_gen_ps_op_intexp),
    PKL_PHASE_PS_OP_HANDLER (PKL_AST_OP_DIV, pkl_gen_ps_op_div),
-   PKL_PHASE_PS_OP_HANDLER (PKL_AST_OP_AND, pkl_gen_ps_op_and),
-   PKL_PHASE_PS_OP_HANDLER (PKL_AST_OP_OR, pkl_gen_ps_op_or),
+   PKL_PHASE_PR_OP_HANDLER (PKL_AST_OP_AND, pkl_gen_pr_op_and),
+   PKL_PHASE_PR_OP_HANDLER (PKL_AST_OP_OR, pkl_gen_pr_op_or),
    PKL_PHASE_PS_OP_HANDLER (PKL_AST_OP_NOT, pkl_gen_ps_op_not),
    PKL_PHASE_PS_OP_HANDLER (PKL_AST_OP_EQ, pkl_gen_ps_op_rela),
    PKL_PHASE_PS_OP_HANDLER (PKL_AST_OP_NE, pkl_gen_ps_op_rela),
